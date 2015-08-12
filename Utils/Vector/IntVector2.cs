5ee6998836ab1b9c3bfa4bfc6e1d033436676498
Andrew Munro <andrew.munro@dubitlimited.com>
2015-08-12 16:41:32 +0100

    Adding vector helpers

---
+++ Utils/Vector/IntVector2.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.Framework.Utils.Vector
+{
+    public struct IntVector2
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
---
+++ Utils/Vector/IntVector3.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Framework.Utils.Vector
+{
+    public struct IntVector3
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+    }
+}
